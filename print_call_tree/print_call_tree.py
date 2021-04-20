5a10b62cee6c8b4733c53a350823667312c8d28b
lihaisu <lihaisu@xiaomi.com>
2021-04-20 19:48:12 +0800

    Fix ring call error

--- print_call_tree/print_call_tree.py
+++ print_call_tree/print_call_tree.py
@@ -6,13 +6,13 @@ node_list = []
 node_dict = {}
 
 n = FlowGraphNode(graph)
-bass_function = bv.get_function_at(0x0100000dc)#write function address here
+bass_function = bv.get_function_at(0x046a8f4)#write function address here
 callers = list(set(bass_function.callers))
 n.lines = bass_function.name
 graph.append(n)
 node_dict[bass_function] = n
 #print(callers)
-
+len_node_dict_old = len(node_dict)
 while(1):
 	new_find_callers_list = []
 	if callers == []:
@@ -29,6 +29,11 @@ while(1):
 					i.start,)]
 		node_dict[i] = n
 		new_find_callers_list = new_find_callers_list + list(set(i.callers))
+	if len(node_dict)>len_node_dict_old:
+		len_node_dict_old = len(node_dict)
+	else:
+		print(len_node_dict_old)
+		break
 	callers =  new_find_callers_list
 
 node_list = node_dict.keys()
@@ -42,3 +47,4 @@ for i in node_list:
 		node_dict[p].add_outgoing_edge(BranchType.UnconditionalBranch,node_dict[i])
 
 show_graph_report("CALL Graph", graph)
+
